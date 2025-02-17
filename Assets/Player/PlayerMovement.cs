d37099b4e4c0641aeb526a28591866b0d83ec485
Sausyman5 <masonwyman4@gmail.com>
2025-02-16 16:18:04 -0800

    Player Faces Mouse

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Camera cam;
+    
 
     [Header("Controls")]
     private float horizontal;
@@ -13,12 +15,14 @@ public class PlayerMovement : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        cam = Camera.main;
     }
 
 
     private void Update()
     {
         InputDetect();
+        FaceMouse();
     }
 
     private void FixedUpdate()
@@ -37,4 +41,13 @@ public class PlayerMovement : MonoBehaviour
     {
         rb.linearVelocity = new Vector2(horizontal, vertical).normalized * speed;
     }
+
+    private void FaceMouse()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = cam.ScreenToWorldPoint(mousePosition);
+        Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+        transform.up = direction;
+    }
+
 }
